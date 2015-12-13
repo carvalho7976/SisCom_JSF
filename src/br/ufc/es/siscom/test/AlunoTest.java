a8cdf41ae71bbc25269886534d4723cbf45fafce
rogerio carvalho <rogeriocarvalhosilva15@gmail.com>
2015-12-13 09:50:53 -0300

    casos de testes criados. adiciondo plugin cobertura

--- src/br/ufc/es/siscom/test/AlunoTest.java
+++ src/br/ufc/es/siscom/test/AlunoTest.java
@@ -2,19 +2,45 @@ package br.ufc.es.siscom.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+import  static org.mockito.Mockito.mock;
+import  static org.mockito.Mockito.when;
+import  static org.mockito.Mockito.doThrow;
+
+import br.ufc.es.siscom.controller.AlunoController;
+import br.ufc.es.siscom.dao.AlunoDAO;
+import br.ufc.es.siscom.model.Aluno;
 
 
 
 public class AlunoTest {
-
 	
-	@Test
-	public void testOk(){
+	AlunoController alunoCtrl;
+	
+	
+	@Before
+	public void create(){
+		alunoCtrl = mock(AlunoController.class);
+		when(alunoCtrl.atualizarAluno()).thenReturn("listarAlunos.xhtml");
 		
-		assertEquals(3, 3);
 	}
 	
+	@Test
+	public void testAlunoCntrAtualizar(){
+		assertEquals("listarAlunos.xhtml", alunoCtrl.atualizarAluno());
+	}
+	
+	
+	/*@Test(expected= Exception.class)
+	public void testExecptionAddAluno(){
+		alunoDao.adicionarAluno(aluno);		
+	}
+*/	
+	/*@Test(expected=IllegalArgumentException.class)
+	public void testExecptionBuscarAlunoPorOrientador(){
+		alunoDao.retornarTodosAlunosNaoMonitores(null);
+	}*/
 /* @Test
 	public void testNaoOk(){
 		assertEquals(3,2);
---
+++ src/br/ufc/es/siscom/test/OrientadorTest.java
@@ -0,0 +1,27 @@
+package br.ufc.es.siscom.test;
+
+import static org.junit.Assert.*;
+import  static org.mockito.Mockito.mock;
+import  static org.mockito.Mockito.when;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.ufc.es.siscom.controller.OrientadorController;
+
+
+
+public class OrientadorTest {
+	OrientadorController orientador;
+	
+	@Before
+	public void create() {
+		orientador = mock(OrientadorController.class);
+		when(orientador.cadastrarOrientador()).thenReturn("listarOrientadores.xhtml");
+	}
+	
+	@Test
+	public void testCadastrarOrientador(){
+		assertEquals("listarOrientadores.xhtml", orientador.cadastrarOrientador());
+	}
+
+}
